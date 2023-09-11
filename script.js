f1d0671bcd46662d088c55ecbbb2763a976be55e
estherpinos <estherpinos1@gmail.com>
2023-09-11 15:04:10 +0200

    Add [fizz, buzz, fizzbuzz in console]

--- script.js
+++ script.js
@@ -5,4 +5,26 @@ for(let i = 1; i<=100; i++){
   const box = document.createElement('div');
   box.classList.add('box');
   boxes.appendChild(box);
+  
+
+  if (i % 15 == 0){
+    box.append("FizzBuzz");
+    console.log("FizzBuzz");
+  }
+  
+  else if (i % 3 == 0){
+    box.append("Fizz");
+    console.log("Fizz");
+  }
+   
+  else if (i % 5 == 0){
+    box.append("Buzz");
+    console.log("Buzz");
+  }
+   
+  else {
+    box.append(i)
+    console.log(i)
+  }
+    
 }
