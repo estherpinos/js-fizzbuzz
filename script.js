7d762fe1446c529c3d31bcfa4041828c6129ebf2
estherpinos <estherpinos1@gmail.com>
2023-09-11 15:12:46 +0200

    Edit [colors and container]

--- script.js
+++ script.js
@@ -10,16 +10,19 @@ for(let i = 1; i<=100; i++){
   if (i % 15 == 0){
     box.append("FizzBuzz");
     console.log("FizzBuzz");
+    box.classList.add('bg-info-subtle');
   }
   
   else if (i % 3 == 0){
     box.append("Fizz");
     console.log("Fizz");
+    box.classList.add('bg-warning');
   }
    
   else if (i % 5 == 0){
     box.append("Buzz");
     console.log("Buzz");
+    box.classList.add('bg-warning-subtle');
   }
    
   else {
